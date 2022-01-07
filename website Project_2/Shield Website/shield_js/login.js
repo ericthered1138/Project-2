3ffcb0433c9d24905d212170b4884c1c113f3cab
dZazulak <94470265+dZazulak@users.noreply.github.com>
2022-01-07 14:26:39 -0600

    Fixed error for incorrect password and changed dao to setFloat not setInt

--- website Project_2/Shield Website/shield_js/login.js
+++ website Project_2/Shield Website/shield_js/login.js
@@ -10,12 +10,12 @@ async function Login(){
         method: "POST",
         cache: "no-cache",
         headers:{"Content-Type": 'application/json'},
-        body:userJSON}).then(response => {return response.json()});
+        body:userJSON});
     
-    console.log(response);
-
-    if(Object.keys(response).length === 5){
-        window.sessionStorage.setItem("userId", response["userId"]);
+        
+    if (response.status == 200){
+        let body = await response.json();
+        window.sessionStorage.setItem("userId", body["userId"]);
         window.location.href = "shield_html/user.html";
     }
     else{
@@ -36,17 +36,20 @@ async function LoginShieldAgent(){
         cache: "no-cache",
         method: "POST",
         headers:{"Content-Type": 'applicaton/json'},
-        body:shieldAgentJSON}).then(response => {return response.json()});
+        body:shieldAgentJSON})
     
     console.log(response);
 
-    if(Object.keys(response).length === 7 && response["handler"] == true){
-        window.sessionStorage.setItem("employeeId", response["employeeId"]);
-        window.location.href = "shield_html/shield_handler.html";
-
-    }else if(Object.keys(response).length === 7){
-        window.sessionStorage.setItem("employeeId", response["employeeId"]);
-        window.location.href = "shield_html/shield_agent.html";
+    if (response.status == 201){
+        let body = await response.json();
+        if (body["handler" == true]){
+            window.sessionStorage.setItem("employeeId", body["employeeId"]);
+            window.location.href = "shield_html/shield_handler.html";
+        }
+        else{
+            window.sessionStorage.setItem("employeeId", body["employeeId"]);
+            window.location.href = "shield_html/shield_agent.html";
+        }
     }
     else{
         alert("Invalid username or password")
@@ -85,32 +88,6 @@ async function CreateUserAccount(){
         alert("Password does not match")
     }
 }
-
-// async function LoginShieldHandler(){
-//     let url = "";
-//     const username = document.getElementById("username");
-//     const password = document.getElementById("password");
-
-//     sessionStorage.setItem("username", username.value);
-//     sessionStorage.setItem("password", password.value);
-//     console.log(username.value);
-//     console.log(password.value);
-//     shieldHandlerJSON = JSON.stringify({"username": username.value.toLowerCase(), "password": password.value});
-//     console.log(shieldHandlerJSON);
-
-//     let respose = await fetch(url, {
-//         method: "POST", 
-//         headers:{"Content-Type": 'application/json'},
-//         body: shieldHandlerJSON}).then(response => {return response.json()});
-
-//     if(response.username == username.value.toLowerCase() && response.passcode == password.value && response.handler == true){
-//         window.location.href = "/shield_handler.html";
-//     }
-//     else{
-//         alert("Invalid username or password")
-//         console.log(response.username, response.password);
-//     }
-// }
 
 
 // // Testing for Create Account window in Login Screen
