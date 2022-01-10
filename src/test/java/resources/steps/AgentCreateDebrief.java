0545c5b3c73d106db843292249138ef8b3a35b21
ericthered1138 <ericjennings955@gmail.com>
2022-01-10 15:32:36 -0600

    updated Alex's tests and the agent page

--- src/test/java/resources/steps/AgentCreateDebrief.java
+++ src/test/java/resources/steps/AgentCreateDebrief.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
@@ -22,38 +23,53 @@ public class AgentCreateDebrief {
 //        TestRunner.driver.get(f.getAbsolutePath());
 //    }
     @When("the agent clicks on the button to start debrief form")
-    public void the_agent_clicks_on_the_button_to_start_debrief_form() throws InterruptedException {
+    public void the_agent_clicks_on_the_button_to_start_debrief_form(){
         // Write code here that turns the phrase above into concrete actions
-        TestRunner.explicitWait.until(ExpectedConditions.elementToBeClickable(TestRunner.agentcreatedebrief.subButton));
-        TestRunner.agentcreatedebrief.subButton.click();
+        TestRunner.explicitWait.until(ExpectedConditions.elementToBeClickable(By.id("subButton")));
+        TestRunner.agentCreateDebrief.subButton.click();
     }
     @When("the agent enters text into the Debrief text box")
     public void the_agent_enters_text_into_the_debrief_text_box() {
         // Write code here that turns the phrase above into concrete actions
-        TestRunner.explicitWait.until(ExpectedConditions.presenceOfElementLocated(By.id("debriefText")));
-        TestRunner.agentcreatedebrief.debrieftext.sendKeys("I accidentally did take their stuff and use it for the avengers");
+        TestRunner.explicitWait.until(ExpectedConditions.elementToBeClickable(By.id("debriefText")));
+        TestRunner.agentCreateDebrief.debrieftext.sendKeys("I accidentally did take their stuff and use it for the avengers");
     }
     @When("the agent enters the date into the Date box")
     public void the_agent_enters_the_date_into_the_date_box() {
         // Write code here that turns the phrase above into concrete actions
-        TestRunner.explicitWait.until(ExpectedConditions.visibilityOf(TestRunner.agentcreatedebrief.dateOfOccurrence));
-        TestRunner.agentcreatedebrief.dateOfOccurrence.sendKeys("12152021");
+        TestRunner.explicitWait.until(ExpectedConditions.visibilityOf(TestRunner.agentCreateDebrief.dateOfOccurrence));
+        TestRunner.agentCreateDebrief.dateOfOccurrence.sendKeys("1212", Keys.RIGHT, "2021");
     }
     @When("the agent enters a location into the location box")
     public void the_agent_enters_a_location_into_the_location_box() {
         // Write code here that turns the phrase above into concrete actions
-        TestRunner.explicitWait.until(ExpectedConditions.visibilityOf(TestRunner.agentcreatedebrief.locationText));
-        TestRunner.agentcreatedebrief.locationText.sendKeys("NYC");
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        TestRunner.explicitWait.until(ExpectedConditions.visibilityOf(TestRunner.agentCreateDebrief.locationText));
+        TestRunner.agentCreateDebrief.locationText.sendKeys("NYC");
     }
     @When("the agent clicks on the submit button")
     public void the_agent_clicks_on_the_submit_button() {
         // Write code here that turns the phrase above into concrete actions
-        TestRunner.explicitWait.until(ExpectedConditions.elementToBeClickable(TestRunner.agentcreatedebrief.submitButton));
-        TestRunner.agentcreatedebrief.submitButton.click();
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        TestRunner.explicitWait.until(ExpectedConditions.elementToBeClickable(TestRunner.agentCreateDebrief.submitButton));
+        TestRunner.agentCreateDebrief.submitButton.click();
     }
     @Then("the agent will submit a new debrief case into the table")
     public void the_agent_will_submit_a_new_debrief_case_into_the_table() {
         // Write code here that turns the phrase above into concrete actions
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         TestRunner.explicitWait.until(ExpectedConditions.titleIs("Shield Agent"));
         String title = TestRunner.driver.getTitle();
         Assert.assertEquals(title, "Shield Agent");
