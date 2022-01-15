ff427f5dd64a609b378d9a890401aff37f42cbc4
ericthered1138 <ericjennings955@gmail.com>
2022-01-15 09:38:20 -0600

    fixing up

--- Planning/testingimagewebsite/image.js
+++ Planning/testingimagewebsite/image.js
@@ -17,3 +17,40 @@ async function getEmployeeImage(){
 }
 
 getEmployeeImage()
+
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
+  
+async function previewFile() {
+  let file    = document.getElementById('portrait_input').files[0];
+  let reader  = new FileReader();
+  let format = "data:image/gif;base64,";
+  let length = format.length
+  let base64gif;
+
+  reader.addEventListener("load", function () {
+    base64gif = reader.result; // your gif in base64 here
+    console.log(base64gif.slice(11, 14));
+
+    if (base64gif.length < 1_000_000 && base64gif.slice(11, 14) === "gif"){
+      fetch(
+        "http://localhost:8080/employee/image/" + "1", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: String(base64gif).slice(length)
+        })
+    
+    }
+    else{
+      alert("File too large")
+    }
+  }, false);
+
+  if (file) {
+    reader.readAsDataURL(file);
+    await delay(2000);
+    getEmployeeImage();
+  }
+}
